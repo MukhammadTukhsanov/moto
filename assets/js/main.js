d8287677118abbcd84f7df1e0fc935256ca299e8
Mukhammad <30554310+MukhammadTukhsanov@users.noreply.github.com>
2024-05-29 18:29:44 +0400

    new changes

---
+++ assets/js/main.js
@@ -0,0 +1,224 @@
+document.getElementById("#elektrosikli");
+document.getElementById("#kvadrosikli");
+document.getElementById("#kabin");
+document.getElementById("#LC320");
+document.getElementById("#LC300");
+document.getElementById("#LC260");
+document.getElementById("#LC200");
+document.getElementById("#LC150");
+
+document.getElementById("#elektrosikli").addEventListener("click", function () {
+  document.querySelectorAll("#elektrosikli").forEach(function (el) {
+    el.style.display = "block";
+  });
+  document.querySelectorAll("#kvadrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kabin").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC320").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC300").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC260").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC200").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC150").forEach(function (el) {
+    el.style.display = "none";
+  });
+});
+
+document.getElementById("#kvadrosikli").addEventListener("click", function () {
+  document.querySelectorAll("#elektrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kvadrosikli").forEach(function (el) {
+    el.style.display = "block";
+  });
+  document.querySelectorAll("#kabin").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC320").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC300").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC260").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC200").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC150").forEach(function (el) {
+    el.style.display = "none";
+  });
+});
+
+document.getElementById("#kabin").addEventListener("click", function () {
+  document.querySelectorAll("#elektrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kvadrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kabin").forEach(function (el) {
+    el.style.display = "block";
+  });
+  document.querySelectorAll("#LC320").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC300").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC260").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC200").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC150").forEach(function (el) {
+    el.style.display = "none";
+  });
+});
+
+document.getElementById("#LC320").addEventListener("click", function () {
+  document.querySelectorAll("#elektrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kvadrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kabin").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC320").forEach(function (el) {
+    el.style.display = "block";
+  });
+  document.querySelectorAll("#LC300").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC260").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC200").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC150").forEach(function (el) {
+    el.style.display = "none";
+  });
+});
+
+document.getElementById("#LC300").addEventListener("click", function () {
+  document.querySelectorAll("#elektrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kvadrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kabin").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC320").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC300").forEach(function (el) {
+    el.style.display = "block";
+  });
+  document.querySelectorAll("#LC260").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC200").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC150").forEach(function (el) {
+    el.style.display = "none";
+  });
+});
+
+document.getElementById("#LC260").addEventListener("click", function () {
+  document.querySelectorAll("#elektrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kvadrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kabin").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC320").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC300").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC260").forEach(function (el) {
+    el.style.display = "block";
+  });
+  document.querySelectorAll("#LC200").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC150").forEach(function (el) {
+    el.style.display = "none";
+  });
+});
+
+document.getElementById("#LC200").addEventListener("click", function () {
+  document.querySelectorAll("#elektrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kvadrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kabin").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC320").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC300").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC260").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC200").forEach(function (el) {
+    el.style.display = "block";
+  });
+  document.querySelectorAll("#LC150").forEach(function (el) {
+    el.style.display = "none";
+  });
+});
+
+document.getElementById("#LC150").addEventListener("click", function () {
+  document.querySelectorAll("#elektrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kvadrosikli").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#kabin").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC320").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC300").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC260").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC200").forEach(function (el) {
+    el.style.display = "none";
+  });
+  document.querySelectorAll("#LC150").forEach(function (el) {
+    el.style.display = "block";
+  });
+});
